d7e429db48f0b693935fd3151194b0a65b4ae9de
Илья Дрозденко <drozdenkoid@gmail.com>
2025-02-28 22:19:10 +0400

    Task2_2 1,2,6 done

--- src/main/java/org/knit/Main.java
+++ src/main/java/org/knit/Main.java
@@ -1,12 +1,13 @@
 package org.knit;
-
-import org.knit.lab2_1.task2_1_3.Task2_1_3;
+import org.knit.lab2_2.task2_2_1.Task2_2_1;
+import org.knit.lab2_2.task2_2_2.Task2_2_2;
+import org.knit.lab2_2.task2_2_6.Task2_2_6;
 
 import java.io.IOException;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws IOException, IllegalAccessException, SQLException {
-        Task2_1_3.Run();
+        Task2_2_6.Run();
     }
 }
---
+++ src/main/java/org/knit/lab2_2/task2_2_2/Restaurant.java
@@ -0,0 +1,28 @@
+package org.knit.lab2_2.task2_2_2;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.TreeMap;
+
+public class Restaurant {
+    private final int capacity = 3;
+    private Queue<String> mealsQueue = new LinkedList<>();
+
+    public synchronized void cook(String name) throws InterruptedException {
+        while (mealsQueue.size() >= capacity) {
+            System.out.println("Подносы заняты, повар ожидает....");
+            wait();
+        }
+        mealsQueue.add(name);
+        System.out.println("Повар приготовил " + name);
+        Thread.sleep(2000);
+        notify();
+    }
+
+    public synchronized void serve() throws InterruptedException {
+        while (mealsQueue.isEmpty()){
+            System.out.println("Повар еще ничего не положил на поднос...");
+            wait();
+        }
+        System.out.println("Официант подал " + mealsQueue.poll());
+    }
+}
